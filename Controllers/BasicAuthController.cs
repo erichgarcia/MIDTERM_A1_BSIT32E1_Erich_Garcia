36b163e30b13ee6162414aa415c7f788fae6d9b0
STUDENT <xyryllematiga@gmail.com>
2026-01-24 09:15:19 +0800

    Add project files.

---
+++ Controllers/BasicAuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MIDTERM_A1_BASICAUTH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasicAuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("You are in");
+        }
+    }   
+}
---
+++ Middleware/BasicAuthMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace MIDTERM_A1_BASICAUTH.Middleware
+{
+    public class BasicAuthMiddleware
+    {
+        public Task Invoke(HttpContext context)
+        {
+            var str = context.Request.Path.Value;
+
+            var authorizatonHeader = context.Request.Headers["Authorization"];
+
+
+            return default;
+        }
+        public BasicAuthMiddleware(RequestDelegate next)
+        {
+            
+        }
+    }
+}
